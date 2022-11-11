16d2c434950a41056de55d1b31ec335c82140c9b
Arti <artitongia.es@gmail.com>
2022-11-11 12:34:58 +0530

    added testcase

---
+++ alert.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+location = "https://demoqa.com/alerts"
+driver.get(location)
+
+# Click on the "Alert" button to generate the Simple Alert
+clickMeButton = driver.find_element(By.ID, 'alertButton')
+clickMeButton.click()
+time.sleep(2)
+
+# Switch the control to the Alert window
+alertBox = driver.switch_to.alert
+# accept the alert
+alertBox.accept()
+
+# close the driver
+driver.close()
